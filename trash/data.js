b23b17cbecfd61d094dce4322f76908cc8da0e25
Jonas <wunderlich.jonas@gmail.com>
2013-10-09 17:16:01 +0200

    trash

---
+++ trash/data.js
@@ -0,0 +1,166 @@
+/*
+ * v_id:  	Eindeutige Id für die Videonummerierung
+ * s_id: 	Startknoten
+ * e_id: 	Endknoten
+ * title: 'Bezeichnung des Videos'
+ *	
+ */
+
+
+
+// Videos
+var videos = [
+	{
+		id: 0,
+		startPos: 0,
+		endPos: 1,
+		title: 'Bibliothek besuchen',
+		text: 'Bechreibung zum video',
+		media: 'fahrtZurBibliothekKlein.mov',
+		reverse: 'fahrtZurBibliothekKleinReverse.mov'
+
+	},
+	{
+		id:1,
+		startPos:1,
+		endPos:1,
+		title:'Freie Bibliothek besuchen',
+		text:'Bechreibung zum video',
+		media:'maoJusBibliothekKlein.mov'
+	},
+	{
+		id: 2,
+		startPos: 1,
+		endPos: 2,
+		title: 'Einen Spaziergang durch das Stadtviertel machen',
+		text: 'Bechreibung zum video',
+		media: '798FahrtZuGallerieKlein.mov'
+	},
+	{
+		id: 3,
+		startPos: 1,
+		endPos: 'Keine Ahnung. Es gibt nach diesem Clip keine weitere Verknüpfung. Also Shuffle oder zurück zum Platz des himmlischen Friedens',
+		title: 'Freund von Mao Ju treffen',
+		text: 'Beschreibung zum video',
+		media: 'PhiloKlein.mov'
+	},
+	{
+		id: 4,
+		startPos: 1,
+		endPos: 3,
+		title: 'Etwas Essen gehen',
+		text: 'Beschreibung zum video',
+		media: 'IntroSHGklein.mov'
+	},
+	{
+		id: 5,
+		startPos: 2,
+		endPos: 3,
+		title: 'Etwas Essen gehen',
+		text: 'Beschreibung zum video',
+		media: 'IntroSHGklein.mov'
+	},
+	{
+		id: 6,
+		startPos: 3,
+		endPos: 4,
+		title: 'Hineingehen',
+		text: 'Beschreibung zum video',
+		media: 'InnenSHGklein.mov'
+	},
+	{
+		id: 7,
+		startPos: 4,
+		endPos: 5,
+		title: 'Mit dem Laptopbenutzer mit Schirmmütze sprechen',
+		text: 'Beschreibung zum video',
+		media: 'FotokünstlerKlein.mov'
+	},
+	{
+		id: 8,
+		startPos: 5,
+		endPos: 6,
+		title: 'Mit dem bebrillten Laptopbenutzer sprechen',
+		text: 'Beschreibung zum video',
+		media: 'PoetRestaurantKlein.mov'
+	},
+	{
+		id: 9,
+		startPos: 6,
+		endPos: 5,
+		title: 'Mit dem Laptopbenutzer mit Schirmmütze sprechen',
+		text: 'Beschreibung zum video',
+		media: 'FotkünstlerKlein.mov'
+	},
+	{
+		id: 10,
+		startPos: 6,
+		endPos: 7,
+		title: 'Fastenden Freund besuchen',
+		text: 'Beschreibung zum video',
+		media: 'HungerkünstlerKlein1.mov'
+	},
+	{
+		id: 11,
+		startPos: 7,
+		endPos: 6,
+		title: 'Mit Johnell sprechen',
+		text: 'Beschreibung zm video',
+		media: 'PoetRestaurantKlein.mov'
+	}
+];
+	
+
+		
+
+// Orte
+var places = [
+	{
+		id: 0,
+		title: 'Platz des Himmlischen Friedens',
+		media: 'himmlischerFieden.jpg',
+		text: 'Beschreibung des Ortes'
+	},
+	{
+		id: 1,
+		title: 'Vor der Bibliothek',
+		media: 'vorDerBiblothek.mov',
+		text: 'Beschreibung des Ortes'
+	},
+	{
+		id: 2,
+		title: 'Vor der Galerie',
+		media: 'Galerie.jpg',
+		text: 'Beschreibung des Ortes'
+	},
+	{
+		id: 3,
+		title: 'Vor dem Restaurant',
+		media:'Restaurant.jpg',
+		text: 'Beschreibung des Ortes'
+	},
+	{	
+		id: 4,
+		title: 'Im Restaurant',
+		media: 'RestaurantInnen.jpg',
+		text: 'Auswahl zwischen den beiden Laptopbenutzern'
+	},
+	{
+		id: 5,
+		title: 'Im Restaurant 2',
+		media: 'Restaurant2.jpg',
+		text: 'Auswahl zwischen dem Besuch des Galeriviertels 798 und dem bebrillten Laptopbesitzer (Die Problematik ist hier, dass wir uns zwar die ganze Zeit im Restaurant befinden, sich nach den Gesprächen aber neue Wege eröffnen, daher die verschiedenen Orte)'
+	},
+	{	
+		id: 6,
+		title: 'Im Restaurant 3',
+		media: 'Restaurant3.jpg',
+		text: 'Auswahl zwischen dem fastenden Freund und dem Laptopbenutzer mit Schirmmütze'
+	},
+	{
+		id: 7,
+		title: 'im Hotelzimmer',
+		media: 'Hotelzimmer.mov',
+		text: 'Beschreibung des Ortes'
+	}
+];
